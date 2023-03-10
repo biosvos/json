840561c0003e88fd08e11273a8a00f075d058eb4
jinsu <realwaterkim@datacommand.kr>
2023-03-10 16:09:52 +0900

    copy 추가

--- json.go
+++ json.go
@@ -1,15 +1,39 @@
 package json
 
-import "strconv"
+import (
+	"encoding/json"
+	"github.com/pkg/errors"
+	"strconv"
+)
 
 func Int(str string) (int64, error) {
-	return strconv.ParseInt(str, 10, 64)
+	ret, err := strconv.ParseInt(str, 10, 64)
+	if err != nil {
+		return 0, errors.WithStack(err)
+	}
+	return ret, nil
 }
 
 func Bool(str string) (bool, error) {
-	return strconv.ParseBool(str)
+	ret, err := strconv.ParseBool(str)
+	if err != nil {
+		return false, errors.WithStack(err)
+	}
+	return ret, nil
 }
 
 func Float(str string) (float64, error) {
-	return strconv.ParseFloat(str, 64)
+	ret, err := strconv.ParseFloat(str, 64)
+	if err != nil {
+		return 0, errors.WithStack(err)
+	}
+	return ret, nil
+}
+
+func Copy(dst any, src string) error {
+	err := json.Unmarshal([]byte(src), dst)
+	if err != nil {
+		return errors.WithStack(err)
+	}
+	return nil
 }
--- json_test.go
+++ json_test.go
@@ -25,3 +25,16 @@ func TestUtilFloat(t *testing.T) {
 	require.NoError(t, err)
 	require.Equal(t, 1.2345, float)
 }
+
+func TestCopy(t *testing.T) {
+	type Kind struct {
+		Name string `json:"name"`
+		Wow  string `json:"wow"`
+	}
+
+	var kind Kind
+	err := Copy(&kind, `{"name":"kind","wow":"good"}`)
+	require.NoError(t, err)
+	require.Equal(t, "kind", kind.Name)
+	require.Equal(t, "good", kind.Wow)
+}
